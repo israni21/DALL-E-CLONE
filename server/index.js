64caf21f19745e7f45809bb2d57bf717b00bcb78
Riya Israni <riya.israni21@gmail.com>
2023-04-22 11:35:44 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ const path = require('express');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
 
 const app = express();
 
-//Enable body parser
 app.use(express.json());
+app.use(cors());
+
+//Enable body parser
 app.use(express.urlencoded({extended: false}));
 
 //Set static folder
